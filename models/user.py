26be6d2e1970fdd924b60a6081906ad9f70f2364
leandro.cordeiro <leandro.marcos@supermercadosbh.com.br>
2024-08-14 17:05:35 -0300

    inicia criação de classe user

---
+++ models/user.py
@@ -0,0 +1,7 @@
+from app import db
+
+class User(db.Model):
+    # id (inte), username (text), password (text)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(20), unique=False, nullable=False)
