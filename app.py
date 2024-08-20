09227a6d34f727fb897cdda174b77389a6cb7db9
leandro.cordeiro <leandro.marcos@supermercadosbh.com.br>
2024-08-20 16:26:09 -0300

    adição de criptografia na senha

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import bcrypt
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
@@ -30,7 +31,7 @@ def login():
         #login
         user = User.query.filter_by(username=username).first()
         
-        if user and user.password == password:
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
             login_user(user)
             print(current_user.is_authenticated)
             return jsonify({'messege': 'Autenticação realizada com sucesso'})
@@ -50,7 +51,8 @@ def create_user ():
     password = data.get('password')
 
     if username and password:
-        user = User(username=username, password=password, role='user')
+        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt()) #Senha criptografada
+        user = User(username=username, password=hashed_password, role='user')
         db.session.add(user)
         db.session.commit()
         return jsonify({'messege': 'Cadastro realizado com sucesso!'})
