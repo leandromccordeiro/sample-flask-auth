d71b8fc21f98eb5104228f669e8c1f734a73cbc7
leandro.cordeiro <leandro.marcos@supermercadosbh.com.br>
2024-08-20 10:57:17 -0300

    criação logout

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from login import login_manager, login_user, current_user
+from login import login_manager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
@@ -35,7 +35,11 @@ def login():
     
     return jsonify({'message':'Credenciais Inválidas'}), 400
 
-
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'messege': "Logout realizado com sucesso!"})
 
 @app.route('/hello-world', methods=['GET'])
 def hello_world():
--- login.py
+++ login.py
@@ -1,2 +1,2 @@
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 login_manager = LoginManager()
