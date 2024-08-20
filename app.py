9fc60d987e2f961e032ea09331a2c3ed10f46cc2
leandro.cordeiro <leandro.marcos@supermercadosbh.com.br>
2024-08-20 09:47:03 -0300

    inicio de criação de login

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask
 from models.user import User
 from database import db
+from login import login_manager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db.init_app(app)
+login_manager.init_app(app)
+
+#view login
+
+#session
+@app.route('//login', methods=['POST'])
+def login():
+    
+
+
 
 @app.route("/hello-world", methods=['GET'])
 def hello_world():
